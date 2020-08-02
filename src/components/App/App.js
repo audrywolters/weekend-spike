ce09a852f03041be486faa2b95c4150f9e4bcd3e
Audry Wolters <blackstar@BLACKSTAR.local>
2020-08-02 18:07:17 -0500

    button click creates a new CoolBox and displays

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { Component } from "react";
+import React, { Component } from 'react';
 import CoolBox from '../CoolBox/CoolBox';
 import './App.css';
 
-function App() {
+class App extends Component {
 
-  return (
-    <>
-      <CoolBox coolText="hello this a string" />
-    </>
-  );
+  state = {
+    coolBoxList: []
+  }
+
+  addCoolBoxClick = () => {
+    
+    const coolBoxes = this.state.coolBoxList;
+
+    this.setState({
+      coolBoxList: coolBoxes.concat( <CoolBox key={ coolBoxes.length } 
+                                              text={ coolBoxes.length } /> )
+    });
+  }
+  
+  render() {    
+    return (
+      <>
+        <button onClick={ this.addCoolBoxClick }>Add a Box!</button>
+        <div>{ this.state.coolBoxList }</div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/CoolBox/CoolBox.js
+++ src/components/CoolBox/CoolBox.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import './CoolBox.css';
 
-
 class CoolBox extends Component {
 
     render() {
         return (
             <div className="coolBox">
-                { this.props.coolText } 
+                { this.props.text } 
             </div>
         );
     }
